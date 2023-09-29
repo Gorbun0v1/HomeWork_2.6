6fa5243071dd1a4f322be35842847dc08ff36510
Gorbun0v1 <joongleboo01@gmail.com>
2023-09-29 13:58:39 +0300

    Up2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //***********************
+        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums.get(i) % 2 != 0) {
+                System.out.println(nums.get(i));
+            }
+        }
+        //***********************
+        List<Integer> nums2 = new ArrayList<>(List.of(3, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        HashSet<Integer> numSet = new HashSet<>(nums2);
+        nums2 = new ArrayList<>(numSet);
+        Collections.sort(nums2);
+        System.out.println(nums2);
+        //***********************
+        List<String> strings = new ArrayList<>(List.of("один", "два", "два", "три", "три", "три"));
+        int count = 1;
+        for (int i = 0; i < strings.size() - 1; i++) {
+            if (strings.get(i).equals(strings.get(i + 1))) {
+                count++;
+            } else {
+                System.out.println(count);
+                count = 1;
+            }
+        }
+        System.out.println(count);
     }
 }
